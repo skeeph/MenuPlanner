70f4d767437cab78fbf8977adcfe8b0f09e0b9a8
Khabib Murtuzaaliev <khabib.murtuzaaliev@gmail.com>
2017-08-27 12:48:11 +0300

    Ng2 completer works

--- src/app/food/food.module.ts
+++ src/app/food/food.module.ts
@@ -5,12 +5,14 @@ import { FoodRoutingModule } from "app/food/food-routing.module";
 import { MenuComponent } from './menu/menu.component';
 import { DayComponent } from './menu/day/day.component';
 import { Ng2CompleterModule } from "ng2-completer";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
     Ng2CompleterModule,
     FoodRoutingModule,
+    FormsModule
   ],
   declarations: [
     MenuComponent,
