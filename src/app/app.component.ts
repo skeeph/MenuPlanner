5b5f701d8c6c5a35bc370d566101cef2221531e8
Khabib Murtuzaaliev <khabib.murtuzaaliev@gmail.com>
2017-09-16 09:40:23 +0300

    Disable firebase auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from "app/recipes/recipe.service";
 import { FoodService } from "app/food/food.service";
+import { AuthService } from 'app/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -15,8 +16,13 @@ export class AppComponent implements OnInit {
 
   constructor(
     private recipeService: RecipeService,
-    private foodService:FoodService
+    private foodService: FoodService,
+    private authService: AuthService,
   ) {
+    if (this.authService.isAuthed()) {
+      this.recipeService.loadRecipes();
+      this.foodService.loadFood();
+    }
 
   }
   title = 'app';
